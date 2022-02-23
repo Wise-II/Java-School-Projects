fc08d0a09d00216adea60cc49613e626a3bc187a
Wise_XXIV <dz.diego5967@outlook.com>
2022-02-23 09:35:10 -0500

    4-8, 4-9 additions/ Revisions

--- MethodCHP4/src/methodchp4/Employee.java
+++ MethodCHP4/src/methodchp4/Employee.java
@@ -9,6 +9,25 @@ package methodchp4;
  *
  * @author Wise-XXIV
  */
-public class Employee {
-    
+public class Employee 
+        
+{
+private int empNum;
+private double empPayRate;
+public void setValues()
+{
+empNum = 111;
+empPayRate = 22.22;
 }
+public void methodThatUsesInstanceAttributes()
+{
+System.out.println("employee number is " + empNum);
+System.out.println("pay rate is " + empPayRate);
+}
+public void methodThatUsesLocalVariables()
+{
+int empNum = 33333;
+double empPayRate = 555.55;
+System.out.println("employee number is " + empNum);
+System.out.println("pay rate is " + empPayRate);
+}}
